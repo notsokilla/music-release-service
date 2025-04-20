5233eb65a942628ed1eeaeafb8e5313cac8c7cb8
Жопи <glicinkz@gmail.com>
2025-04-20 22:33:36 +0300

    Обновление логики расчета отчетов: добавлена обработка всех финансовых колонок и исправлен расчет долей

--- render-build.sh
+++ render-build.sh
@@ -4,11 +4,16 @@ cd frontend
 npm install
 npm run build
 
-# Подготовка статики
+# Копирование статики
 cd ..
 mkdir -p backend/static
 cp -r frontend/build/* backend/static/
 
 # Бекенд
 cd backend
-pip install -r requirements.txt
+if [ -f "requirements.txt" ]; then
+  pip install -r requirements.txt
+else
+  echo "Error: requirements.txt not found!" >&2
+  exit 1
+fi
