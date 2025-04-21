02c89b2965431b332ed569fede0d40f7cd761f2e
Жопи <glicinkz@gmail.com>
2025-04-21 10:51:15 +0300

    Обновление логики расчета отчетов: добавлена обработка всех финансовых колонок и исправлен расчет долей

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,10 @@ const App = () => {
         <h1 className="header">Система управления музыкальными отчетами</h1>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/add-split" element={<AddSplit />} />
-          <Route path="/get-splits" element={<GetSplits />} />
-          <Route path="/delete-splits" element={<DeleteSplits />} />
-          <Route path="/generate-reports" element={<GenerateReports />} />
+          <Route path="/add-split/" element={<AddSplit />} />
+          <Route path="/get-splits/" element={<GetSplits />} />
+          <Route path="/delete-splits/" element={<DeleteSplits />} />
+          <Route path="/generate-reports/" element={<GenerateReports />} />
         </Routes>
       </div>
     </Router>
